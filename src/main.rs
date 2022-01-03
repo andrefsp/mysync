11bfec86a8988871807c4e2b7e5bf889837de844
andrefsp <andrefsp@gmail.com>
2022-01-03 01:39:32 +0000

    hooked methods

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::sync::{Arc, Mutex};
+use std::net::SocketAddr;
 
 use tokio;
 use futures::future;
@@ -8,31 +9,19 @@ use mysync::service;
 use mysync::persistence::{DB, Repo};
 
 
-async fn start_svc() { 
-
-    let db = DB::new();
-   
-    // Repo must be moved into the Mutex::new()
-    // Mutex is than moved into an Arc (Atomic Reference counter)
-    let repo = Repo::new(Box::new(db));
-    
-    let svc = service::Svc::new(Arc::new(Mutex::new(repo)));
-
-    service::start(svc).await;    
-}
-
-async fn start(svc: &Arc<Mutex<Repo>>) {
+async fn add_cars(repo: &Arc<Mutex<Repo>>) {
     let mut spawns = Vec::new();
     let mut x = 1;
+
     while x <= 10 {
-        let svc = Arc::clone(&svc); // or let svc = svc.clone();
+        let repo = repo.clone();
         let f = tokio::spawn(async move {
             let b = format!("brand-{}", x);
 
             let car = Car::new(String::from(b));
-            
-            let mut svc = svc.lock().unwrap();
-            svc.add(car);
+
+            let mut repo = repo.lock().unwrap();
+            repo.add(car);
         });
         spawns.push(f);
         x = x + 1;
@@ -41,8 +30,26 @@ async fn start(svc: &Arc<Mutex<Repo>>) {
     future::join_all(spawns).await;
 }
 
+
+async fn start_svc() {
+
+    let db = DB::new();
+
+    // Repo must be moved into the Mutex::new()
+    // Mutex is than moved into an Arc (Atomic Reference counter)
+    let repo = Arc::new(Mutex::new(Repo::new(Box::new(db))));
+
+    add_cars(&repo).await;
+
+    let svc = service::Svc::new(repo);
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    service::start(svc, addr).await.unwrap();
+}
+
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Sync + Send>> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     start_svc().await;
     Ok(())
 }
--- src/service.rs
+++ src/service.rs
@@ -6,9 +6,8 @@ use hyper::Request;
 use hyper::Response;
 use hyper::Server;
 use hyper::body::Body;
-//use hyper::body::HttpBody;
+
 use hyper::service::{make_service_fn, service_fn};
-use hyper::server::conn::AddrStream;
 
 use super::persistence::Repo;
 use super::models::Car;
@@ -23,54 +22,54 @@ pub struct Svc {
 impl Svc {
 
     pub async fn get_car(&self, _req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-        let resp = Response::builder();
         let mut repo = self.repo.lock().unwrap();
 
         let count = repo.count();
         let b = Body::from(format!("{}", count));
+
+        let resp = Response::builder();
         Ok(resp.status(200).body(b).unwrap())
     }
 
-    pub async fn put_car(&self, req: Request<Body>) ->  Result<Response<Body>, hyper::Error> { 
+    pub async fn put_car(&self, req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+
         let body = req.into_body();
 
-        let bytes = hyper::body::to_bytes(body).await?; 
-
+        let bytes = hyper::body::to_bytes(body).await?;
         let payload = str::from_utf8(&bytes).unwrap().to_string();
 
         let car = Car::from_json(payload);
-        
+
         let mut repo = self.repo.lock().unwrap();
-        
+
         repo.add(car);
 
         let resp = Response::builder();
         Ok(resp.status(200).body("PUT CAR".into()).unwrap())
     }
 
+    // `handle` method takes ownership of the whole struct.
+    pub async fn handle(self, req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+        match (req.method(), req.uri().path()) {
+            (&hyper::Method::GET, "/") => self.get_car(req).await,
+            (&hyper::Method::POST, "/") => self.put_car(req).await,
+
+            _ => Ok(Response::new(Body::from("NOT FOUND")) ),
+        }
+    }
+
     pub fn new(repo: Arc<Mutex<Repo>>) -> Svc {
         Svc{
-            repo
+            repo,
         }
     }
 }
 
 
-pub async fn handle(svc: Svc, req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    let resp = match (req.method(), req.uri().path()) {
-        (&hyper::Method::GET, "/") => svc.get_car(req).await,
-        (&hyper::Method::POST, "/") => svc.put_car(req).await,
-
-        _ => Ok(Response::new(Body::from("NOT FOUND")) ),
-    };
-    resp
-}
-
-
-pub async fn start(svc: Svc) {
+pub async fn start(svc: Svc, addr: SocketAddr) -> Result<(), hyper::Error>{
 
     // A `MakeService` that produces a `Service` to handle each connection.
-    let make_service = make_service_fn(move |_: &AddrStream| {
+    let make_service = make_service_fn(move |_| {
         // We have to clone the context to share it with each invocation of
         // `make_service`. If your data doesn't implement `Clone` consider using
         // an `std::sync::Arc`.
@@ -78,16 +77,17 @@ pub async fn start(svc: Svc) {
 
         // Create a `Service` for responding to the request.
         let service = service_fn(move |req| {
-            handle(svc.clone(), req)
+            svc.clone().handle(req)
         });
 
         // Return the service to hyper.
         async move { Ok::<_, hyper::Error>(service) }
     });
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-
     let server = Server::bind(&addr).serve(make_service);
-    
-    server.await;
+
+    match server.await {
+        Err(err) => Err(hyper::Error::from(err)),
+        _ => Ok(()),
+    }
 }
